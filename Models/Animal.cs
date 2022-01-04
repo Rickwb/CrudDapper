862cab378a908904c0aa0b0bc86816dd06d2efcd
Rickwb <66922065+Rickwb@users.noreply.github.com>
2022-01-04 08:23:38 -0300

    Add project files.

---
+++ Models/Animal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudDapper.NewFolder
+{
+    public class Animal
+    {
+        public Animal(string name, string race, string family, int age, bool isDomestic)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Race = race;
+            Family = family;
+            Age = age;
+            IsDomestic = isDomestic;
+        }
+        public Animal(Guid id, string name, string race, string family, int age, bool isDomestic)
+        {
+            Id = id;
+            Name = name;
+            Race = race;
+            Family = family;
+            Age = age;
+            IsDomestic = isDomestic;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Family { get; set; }
+        public int Age { get; set; }
+        public bool IsDomestic { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using CrudDapper.NewFolder;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace CrudDapper
+{
+    class Program
+    {
+        private const string stringConnection = "";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private async Task<bool> InsertAnimal(Animal animal)
+        {
+            await using var conn = new SqlConnection(stringConnection);
+
+            var sql = "@INSERT INTO ANIMAL (id,name,race,family,age,IsDomestic)" +
+                "VALUES (@IdAnimal,@Name,@Race,@Family,@Age,@Domestic);";
+            var result = await conn.ExecuteAsync(sql, animal);
+
+            if (result == 1)
+                return true;
+
+            return false;
+        }
+        private async Task<bool> UpdateAnimal(Animal animal)
+        {
+            await using var conn = new SqlConnection(stringConnection);
+
+            var sql = "@UPDATE ANIMAL SET (name=@Name,race=@Race,family=@Family,age=@Age,IsDomestic=@Domestic)" +
+                "WHERE id=@IdAnimal;";
+            var result = await conn.ExecuteAsync(sql, animal);
+
+            if (result == 1)
+                return true;
+
+            return false;
+        }
+
+        private async Task<bool> DeleteAnimal(Animal animal)
+        {
+            await using var conn = new SqlConnection(stringConnection);
+
+            var sql = "@DELETE FROM ANIMAL WHERE id=@IdAnimal; ";
+
+            var result = await conn.ExecuteAsync(sql, animal.Id);
+
+            if (result == 1)
+                return true;
+
+            return false;
+        }
+
+    }
+}
